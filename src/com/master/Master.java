05e7cdd2bd6e7c3938c58a499deb1dea00f225dd
Rick Ley <fley@usc.edu>
2017-04-11 21:29:05 -0700

    Only have to implement GetHandleForAppend for the Record methods

--- src/com/master/Master.java
+++ src/com/master/Master.java
@@ -10,62 +10,74 @@ public class Master {
 	
 	
 	ArrayList<String> namespace; //Ordered list of FileHandles(String)
-	HashMap<FileHandle, ArrayList<String>> chunkLists;	//Map from FileHandle to list of chunkHandle's
+	HashMap<String, ArrayList<String>> chunkLists;	//Map from FileHandle to list of chunkHandle's
 	HashMap<String, String> chunkLocations; //Map from chunkHandle to chunkServer IPs
+	HashMap<String, Integer> remainingChunkSpace; // How much space is left in last chunk of a file
+	
+	ArrayList<Socket> chunkserverConnections;
+	int currChunkserver;
 	
 	//Locks
 	
-	//Write networking
+	
 	
 	public Master(int portNumber, String hostname)
 	{
 		namespace = new ArrayList<String>();
-		chunkLists = new HashMap<FileHandle, ArrayList<String>>();
+		chunkLists = new HashMap<String, ArrayList<String>>();
 		chunkLocations = new HashMap<String, String>();
+		currChunkserver = 0;
+		//Write networking
 	}
 	
 	//Verify file handle and chunk handle
 	
 	boolean VerifyFileHandleAndChunkHandle(String fileHandle, String chunkHandle){
-		
-		
-		
-		
-		return true;
+		return !chunkLists.containsKey(fileHandle) ||
+				chunkLists.get(fileHandle).indexOf(chunkHandle) >= 0;
 	}
 	
 	/*
-	 * Gets
+	 * Assumes file and chunk handle have already been validated
 	 */
-	String GetNextChunkHandle(String ChunkHandle){
-		
-		
-		
-		return "";
+	String GetNextChunkHandle(String fileHandle, String ChunkHandle){
+		ArrayList<String> list = chunkLists.get(fileHandle);
+		int index = list.indexOf(ChunkHandle) + 1;
+		return index < list.size() ? list.get(index) : null;
 	}
 	
-	String GetPreviousChunkHandle(String ChunkHandle){
-		
-		
-		return "";
+	String GetPreviousChunkHandle(String fileHandle, String ChunkHandle){
+		ArrayList<String> list = chunkLists.get(fileHandle);
+		int index = list.indexOf(ChunkHandle) - 1;
+		return index > 0 ? list.get(index) : null;
 	}
 	
 	String GetLastChunkHandleOfAFile(String FileHandle){
-		
-		
-		return "";
+		ArrayList<String> list = chunkLists.get(FileHandle);
+		return list.size() > 0 ? list.get(list.size() - 1) : null;
 	}
 	
 	String GetFirstChunkHandleForFile(String FileHandle){
-		
-		
-		return "";
+		ArrayList<String> list = chunkLists.get(FileHandle);
+		return list.size() > 0 ? list.get(0) : null;
 	}
 	
-	String GetHandleHandleForAppend(String FileHandle){
-		
-		
-		return "";
+	String GetHandleHandleForAppend(String FileHandle, Integer payloadSize){
+		if(payloadSize < remainingChunkSpace.get(FileHandle))
+		{
+			ArrayList<String> list = chunkLists.get(FileHandle);
+			return list.get(list.size() - 1);
+		}
+		else
+		{
+			// Call a chunkserver to create a chunk
+			// Rotate the chunkserver to create new chunks
+			// add the handle to the list for this file
+			// reset the remaining space to be chunksize - payloadsize
+			// payload size already validated?
+			// return chunk handle
+			return null;
+		}
 	}
 	
 	
